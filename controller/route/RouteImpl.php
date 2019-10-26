f095b3debcaca45ec61c2ba1333413604701f6ba
Bruno Silva <brunoosilvas@gmail.com>
2019-10-26 13:26:18 -0300

    dev icelus

--- controller/route/RouteImpl.php
+++ controller/route/RouteImpl.php
@@ -17,34 +17,41 @@ use icelus\util\Classes;
 
 class RouteImpl implements Route
 {	
+    const KEY_MODULE = "module";
+    const KEY_CONTROLLER = "controller";
+    const KEY_METHOD = "method";
+    const KEY_PARAM = "param";
+    const KEY_VIEW = "view";
+    const KEY_CLASS = "class";
+    
     const CONTROLLER_PATH = "/controller/";
     const CONTROLLER_PATH_VIEW = "/public/view/";
     const CONTROLLER_METHOD = "action";
-	
+
     private $config;
     private $factory;
     private $controller;
 
     public function __construct() 
     {		
-        $this->config = array("module" => $this->module(),
-            "controller" => $this->controller(),
-            "method" => $this->method(),
-            "param" => $this->param(),
-            "view" => $this->view()
+        $this->config = array(RouteImpl::KEY_MODULE => $this->module(),
+            RouteImpl::KEY_CONTROLLER => $this->controller(),
+            RouteImpl::KEY_METHOD => $this->method(),
+            RouteImpl::KEY_PARAM => $this->param(),
+            RouteImpl::KEY_VIEW => $this->view()
         );
     }
 	
     public function intercept() 
     {		
-        $controller = $this->config["controller"];
+        $controller = $this->config[RouteImpl::KEY_CONTROLLER];
 
         $this->factory = new FactoryController($controller);
         $this->controller = $this->factory->instantiate();
 		
         if ($this->controller instanceof \icelus\controller\ActionController)
         {
-            $view = $this->config["view"];
+            $view = $this->config[RouteImpl::KEY_VIEW];
             $this->controller->buildViewManager($view);	
         }
 		
@@ -53,21 +60,21 @@ class RouteImpl implements Route
 
     public function dispatch() 
     {
-        $method = $this->config["method"];
-        $param = $this->config["param"];
+        $method = $this->config[RouteImpl::KEY_METHOD];
+        $param = $this->config[RouteImpl::KEY_PARAM];
         $this->factory->execute($method, $param);
     }
     
     private function module()
     {
-        $module = Request::get("module");
+        $module = Request::get(RouteImpl::KEY_MODULE);
         return $module;
     }
 	
     private function controller() 
     {
         $module = $this->module();
-        $class = Request::get("class");
+        $class = Request::get(RouteImpl::KEY_CLASS);
         $class = Classes::class($class);
 
         $controller = $module;
@@ -79,7 +86,7 @@ class RouteImpl implements Route
 	
     private function method() 
     {
-        $method = Request::get("method");
+        $method = Request::get(RouteImpl::KEY_METHOD);
         $method = Classes::method($method);
 
         return $method == null ? RouteImpl::CONTROLLER_METHOD : $method;
@@ -87,14 +94,14 @@ class RouteImpl implements Route
 	
     private function param() 
     {
-        $param = Request::get("param");
+        $param = Request::get(RouteImpl::KEY_PARAM);
         return $param;
     }
 
     private function view() 
     {
         $module = $this->module();
-        $class = Request::get("class");
+        $class = Request::get(RouteImpl::KEY_CLASS);
 
         $view = $module;
         $view .= RouteImpl::CONTROLLER_PATH_VIEW;
